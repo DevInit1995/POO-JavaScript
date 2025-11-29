9eb963c82e43b1bcd7c6d7fc2131fd5179454af3
Jhon Santos <kennedyalves871@gmail.com>
2025-11-29 13:29:15 -0300

    Seleção de serviços e peças

---
+++ cadastro-oficina/js/cadastroServicosPecas.js
@@ -0,0 +1,14 @@
+class CadastroServicosPecas {
+    paginaCadastroServicosPecas = () => {
+        const menu = document.getElementById("menu");
+        const urlParaAbrir = "menu.html";
+
+        menu.addEventListener("click", function() {
+            window.open(urlParaAbrir, "_blank")
+        });
+    }
+
+}
+
+cadastroServicosPecas = new CadastroServicosPecas();
+cadastroServicosPecas.paginaCadastroServicosPecas();
