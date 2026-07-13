f4b843d477320cd81e1c0d8b87a681063cba51f1
Jhon Santos <kennedyalves871@gmail.com>
2026-07-13 18:08:25 -0300

    Herança entre a classe Campo e CNPJ no arquivo fornecedores

--- frontend/js/fornecedores.js
+++ frontend/js/fornecedores.js
@@ -74,7 +74,6 @@ class CNPJ extends Campo {
         if(cnpj.length !== 14) return false;
         return cnpj.replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, '$1.$2.$3/$4-$5');
     }
-    
 }
 
 const cnpj = new CNPJ();
