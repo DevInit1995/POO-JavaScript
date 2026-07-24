460a9d86e901d1de613292ee3e78bc2c5ffeb092
Jhon Santos <kennedyalves871@gmail.com>
2026-07-24 20:34:59 -0300

    feat(Menu): adiciona nav e footer

--- frontend/js/gerente.js
+++ frontend/js/gerente.js
@@ -1,5 +1,12 @@
-class Gerente {
-
+class Gerente extends Funcionario {
+    constructor(id, nomeCompleto, sexo, dataNascimento) {
+        super(
+            id,
+            nomeCompleto,
+            sexo,
+            dataNascimento,
+        );
+    }
 }
 
 const gerente = new Gerente();
